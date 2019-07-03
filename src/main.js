153af31af4f6f8a2b5a6e8e5c3b083d1037b0184
Vadim Lavorchuk <lavorchuk.vadim@digitalsuits.co>
2019-07-03 19:27:42 +0300

    Added firebase and user auth with it

--- src/main.js
+++ src/main.js
@@ -1,11 +1,15 @@
 import Vue from 'vue';
 import App from './App.vue';
 import router from './router';
-import store from './store';
+import store from './store/store';
 
 import Vuelidate from 'vuelidate';
 import VueMaterial from 'vue-material';
 import dateFilter from './filters/date.filter';
+
+import firebase from 'firebase/app';
+import 'firebase/auth';
+import 'firebase/database';
 
 Vue.config.productionTip = false;
 
@@ -14,8 +18,25 @@ Vue.use(VueMaterial);
 
 Vue.filter('date', dateFilter);
 
-new Vue({
-  router,
-  store,
-  render: h => h(App),
-}).$mount('#app');
+firebase.initializeApp({
+  apiKey: "AIzaSyAYaB4Wl_kFV_N4M7TMiHmGdHW8-i6FpzI",
+  authDomain: "vue-crm-b6f5e.firebaseapp.com",
+  databaseURL: "https://vue-crm-b6f5e.firebaseio.com",
+  projectId: "vue-crm-b6f5e",
+  storageBucket: "",
+  messagingSenderId: "1067677790584",
+  appId: "1:1067677790584:web:08d7b934a40d529f"
+});
+
+let app;
+
+firebase.auth().onAuthStateChanged(() => {
+  if(!app) {
+    app = new Vue({
+      router,
+      store,
+      render: h => h(App),
+    }).$mount('#app')
+  }
+});
+
