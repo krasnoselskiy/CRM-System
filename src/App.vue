d2fe904d28745fea83a91aa3b565c8cdc29cbb70
Vadim Lavorchuk <lavorchuk.vadim@digitalsuits.co>
2019-07-03 14:32:22 +0300

    Add styles and minor fixes

--- src/App.vue
+++ src/App.vue
@@ -7,23 +7,22 @@
 </template>
 
 <script>
-import EmptyLayout from '@/layouts/EmptyLayout'
-import MainLayout from '@/layouts/MainLayout'
+  import EmptyLayout from '@/layouts/EmptyLayout'
+  import MainLayout from '@/layouts/MainLayout'
 
-export default {
-  computed: {
-    layout() {
-      return (this.$route.meta.layout || 'empty') + '-layout'
+  export default {
+    computed: {
+      layout() {
+        return (this.$route.meta.layout || 'empty') + '-layout'
+      }
+    },
+    components: {
+      EmptyLayout, MainLayout
     }
-  },
-  components: {
-    EmptyLayout, MainLayout
   }
-}
 </script>
 
-
 <style lang="scss">
-@import '~materialize-css/dist/css/materialize.min.css';
-@import 'assets/index.css';
+  @import '~materialize-css/dist/css/materialize.min.css';
+  @import 'assets/index.css';
 </style>
