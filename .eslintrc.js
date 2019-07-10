4645870dccfc111252d3c21db85a82a523ecb453
Vadim Lavorchuk <lavorchuk.vadim@digitalsuits.co>
2019-07-10 12:01:45 +0300

    Minor fixes

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,1 +1,11 @@
-/* eslint-disable */
+module.exports = {
+  extends: [
+    // add more generic rulesets here, such as:
+    // 'eslint:recommended',
+    'plugin:vue/recommended'
+  ],
+  rules: {
+    // override/add rules settings here, such as:
+    // 'vue/no-unused-vars': 'error'
+  }
+}
